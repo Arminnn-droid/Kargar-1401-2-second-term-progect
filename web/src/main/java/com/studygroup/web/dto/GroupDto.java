ca85d4755ef5533cb87211890e664c1f05f851a9
ArminMalekM <arminmalekm44@gmail.com>
2023-06-10 03:45:16 -0700

    Group Dto

---
+++ web/src/main/java/com/studygroup/web/dto/GroupDto.java
@@ -0,0 +1,17 @@
+package com.studygroup.web.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class GroupDto {
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String context;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
