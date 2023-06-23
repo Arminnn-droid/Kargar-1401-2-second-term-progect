92f24b49fb9882a9885b28fd30bba573fbb9c4a5
ArminMalekM <arminmalekm44@gmail.com>
2023-06-23 03:34:58 -0700

    Login

--- web/src/main/java/com/studygroup/web/controller/AuthController.java
+++ web/src/main/java/com/studygroup/web/controller/AuthController.java
@@ -31,11 +31,11 @@ public class AuthController {
                                BindingResult result, Model model) {
             UserEntity existingUserEmail = userService.findByEmail(user.getEmail());
             if(existingUserEmail != null && existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
-                result.rejectValue("email", "There is already a user with this email/username");
+                return "redirect:/register?fail";
             }
             UserEntity existingUserUsername = userService.findByUsername(user.getUsername());
             if(existingUserUsername != null && existingUserUsername.getUsername() != null && !existingUserUsername.getUsername().isEmpty()) {
-                result.rejectValue("username", "There is already a user with this email/username");
+                return "redirect:/register?fail";
             }
             if(result.hasErrors()) {
                 model.addAttribute("user", user);
@@ -44,5 +44,11 @@ public class AuthController {
             userService.saveUser(user);
             return "redirect:/clubs?success";
         }
+
+        @GetMapping("/login")
+        public String loginPage(){
+            return "login";
+        }
+
 
 }
