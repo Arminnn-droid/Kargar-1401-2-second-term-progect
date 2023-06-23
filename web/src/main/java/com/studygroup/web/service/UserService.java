b09103cf072465755b9c9911bd93506a79ec9466
ArminMalekM <arminmalekm44@gmail.com>
2023-06-23 02:53:57 -0700

    Register

--- web/src/main/java/com/studygroup/web/service/UserService.java
+++ web/src/main/java/com/studygroup/web/service/UserService.java
@@ -1,7 +1,11 @@
 package com.studygroup.web.service;
 
 import com.studygroup.web.dto.RegistrationDto;
+import com.studygroup.web.models.UserEntity;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
 }
--- web/src/main/java/com/studygroup/web/service/impl/UserServiceImpl.java
+++ web/src/main/java/com/studygroup/web/service/impl/UserServiceImpl.java
@@ -1,2 +1,48 @@
-package com.studygroup.web.service.impl;public class UserServiceImpl {
+package com.studygroup.web.service.impl;
+
+import com.studygroup.web.dto.RegistrationDto;
+import com.studygroup.web.models.Role;
+import com.studygroup.web.models.UserEntity;
+import com.studygroup.web.repository.RoleRepository;
+import com.studygroup.web.repository.UserRepository;
+import com.studygroup.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository repository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    private RoleRepository repository;
+
+    @Override
+    public void saveUser(RegistrationDto registrationDto) {
+        UserEntity user = new UserEntity();
+        user.setUsername(registrationDto.getUsername());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(registrationDto.getPassword());
+        Role role = roleRepository.findByName("USER");
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
